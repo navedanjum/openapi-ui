bbd423ac06450c10811221d68bed4778a2e6cec0
nansari <navedanjum.ansari1@gmail.com>
2022-10-24 00:39:08 +0300

    html-templating and swagger script initialized

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 download and copy swagger UI /dist content from swagger UI latest from github
 See instructuction here : https://github.com/swagger-api/swagger-ui/releases/tag/v4.15.0 
 
+# TODOs'
+1. Refactor - Build
+2. Inject html view
+3. Swagger UI initializer settings
+4. README Update
--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    // initialize to view the difference between open-api spec and those generated using this app
+    url: 'https://raw.githubusercontent.com/twilio/twilio-oai/main/spec/json/twilio_accounts_v1.json',
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
